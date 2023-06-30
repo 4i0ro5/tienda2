7081d791f8a55f003f2de40709564e2f32ff77be
4i0ro5 <tio.guta.13@gmail.com>
2023-06-29 21:19:30 -0500

    encabezados de la api agregados

--- src/app.js
+++ src/app.js
@@ -5,6 +5,14 @@ import registroRoutes from './routes/registro.routes.js'
 const app  = express()
 
 app.use(express.json())
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5500'); // Cambia '*' por el dominio permitido para acceder a la API
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
 app.use(indexRoutes)
 
 app.use("/api", registroRoutes)
